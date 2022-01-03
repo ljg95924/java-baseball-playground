0ccc6facdfecb136e508150287561dec7f99c09c
ljg95 <ljg95924@github.com>
2022-01-04 01:04:39 +0900

    Feat: 메소드 추가

--- src/main/java/bullsandcows/InputView.java
+++ src/main/java/bullsandcows/InputView.java
@@ -4,4 +4,7 @@ public class InputView {
     public void InputVeiw() {
         System.out.println("숫자야구 게임입니다. \n 숫자를 입력해주세요: ");
     }
+    public void ReVeiw(){
+        System.out.println("숫자를 입력해주세요: ");
+    }
 }
