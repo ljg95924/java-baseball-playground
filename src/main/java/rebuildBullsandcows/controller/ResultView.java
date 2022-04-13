7e254e3abcb8bc6dde414d3774b00f5f47cf3a43
ljg95924 <ljg95924@github.com>
2022-04-13 17:54:02 +0900

    Feat: ResultView

---
+++ src/main/java/rebuildBullsandcows/controller/ResultView.java
@@ -0,0 +1,8 @@
+package rebuildBullsandcows.controller;
+
+public class ResultView {
+    public static void endView(){
+        System.out.println("3개의 숫자를 맞히셨습니다! 게임종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
